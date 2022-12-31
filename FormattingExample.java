ff6c4a27ef9fe917ed01006c297bbe8c948f3017
ljsnell <ljsnell@users.noreply.github.com>
2022-12-31 17:45:57 -0600

    Update FormattingExample.java

--- FormattingExample.java
+++ FormattingExample.java
@@ -1,6 +1,6 @@
 import java.text.NumberFormat;
 import java.util.Scanner;
-public class MyClass {
+public class FormattingExample {
 	static Scanner userinput = new Scanner(System.in);
 	public static void main(String[] args) {
 		int n = 56; 

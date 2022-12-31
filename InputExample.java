46975f335cf0c28b62925c93fbb42f1781ee4538
ljsnell <ljsnell@users.noreply.github.com>
2022-12-31 17:38:33 -0600

    Create InputExample.java

---
+++ InputExample.java
@@ -0,0 +1,19 @@
+package myproject;
+
+import java.util.Scanner;
+public class MyClass {
+	static Scanner userinput = new Scanner(System.in);
+	public static void main(String[] args) {
+		int x;
+		x = 56; // this illustrates assignment/hard coding
+		System.out.println("The assigned number is: " + x);
+		
+		System.out.print("Enter a Number -->");
+		x = userinput.nextInt();  // this illustrates user input
+		userinput.nextLine();
+		System.out.println("The inputed number is: " + x);
+
+		x = ((int) (Math.random()*100 + 1));  //this illustrates random input
+		System.out.println("The random number is: " + x);
+	}
+}
